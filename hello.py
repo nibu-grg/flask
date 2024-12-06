32f4e9ea6cabd613cdf5b47a4a4a50b76fedeb51
nibu-grg <20047500@mydbs.ie>
2024-12-06 15:06:32 +0530

    Changes

--- hello.py
+++ hello.py
@@ -1,12 +1,9 @@
-from flask import Flask, jsonify, request 
+from flask import Flask
+app = Flask(__name__)
 
-app = Flask(__name__) 
+@app.route("/")
+    def hello():
+        return "Hello World!"
 
-
-@app.route('/', methods=['GET']) 
-def helloworld(): 
-	return("Hello World")
-
-
-if __name__ == '__main__': 
-	app.run(debug=True) 
+if __name__ == "__main__":
+    app.run()
