61803a58ef38ce8162a7f2ba18abc59906209fd9
nibu-grg <20047500@mydbs.ie>
2024-12-06 14:23:53 +0530

    hello world

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request 
+
+app = Flask(__name__) 
+
+
+@app.route('/', methods=['GET']) 
+def helloworld(): 
+	return("Hello World")
+
+
+if __name__ == '__main__': 
+	app.run(debug=True) 
